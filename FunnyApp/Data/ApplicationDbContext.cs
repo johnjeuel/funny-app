1e30e934ab640b291588579f061908a14db49e5a
Jeuel Joaquin <jeuel.joaquin13@gmail.com>
2022-05-17 23:29:34 +0800

    finished proj

---
+++ FunnyApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using FunnyApp.Models;
+
+namespace FunnyApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<FunnyApp.Models.Joke>? Joke { get; set; }
+
+    }
+}
---
+++ FunnyApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FunnyApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
